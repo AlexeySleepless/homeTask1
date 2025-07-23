19185c8ec241d0064a66b26efba21372d95efb56
Alexey <avd5522@gmail.com>
2025-07-23 16:43:32 +0300

    Убрана работа с localstorage

--- src/entities/task/libs/taskUtils.ts
+++ src/entities/task/libs/taskUtils.ts
@@ -1,8 +1,6 @@
 import { categoryValues, priorityValues, statusValues } from "../../tag/models/types";
 import type { ITask } from "../model";
 
-const keyOfTasksStorage = "task";
-
 export function EmptyTask(): ITask{
     return {
         id: Date.now(),
@@ -13,22 +11,4 @@ export function EmptyTask(): ITask{
         status: statusValues.TODO,
         priority: priorityValues.LOW
     }
-}
-
-
-export function getTaskFromLocalStorage(): ITask[]{
-    const storageString = localStorage.getItem(keyOfTasksStorage);
-    if(!storageString){
-        return [];
-    }
-    const tasks = JSON.parse(storageString)
-    if(!Array.isArray(tasks)){
-        return [];
-    }
-    return tasks
-}
-
-export function setTaskLocalStorage(tasks: ITask[]): void{
-    const stringifyTasks = JSON.stringify(tasks);
-    localStorage.setItem(keyOfTasksStorage, stringifyTasks);
 }
